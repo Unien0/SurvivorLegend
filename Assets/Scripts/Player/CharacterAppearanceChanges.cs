5647041ea751cf5715d939cef5033cbc5e74feb9
UNIEN <unien7415@gmail.com>
2024-03-12 22:57:57 +0900

    【添加】服装更换脚本（未完

--- Assets/Scripts/Player/CharacterAppearanceChanges.cs
+++ Assets/Scripts/Player/CharacterAppearanceChanges.cs
@@ -7,6 +7,28 @@ using UnityEngine;
 /// </summary>
 public class CharacterAppearanceChanges : MonoBehaviour
 {
+    [Header("帽子")]
+    public SpriteRenderer head;
+    [Header("头发")]
+    public SpriteRenderer hair;
+    [Header("上衣")]
+    public SpriteRenderer body;
+    [Header("手套")]
+    public SpriteRenderer gloves;
+    [Header("裤子")]
+    public SpriteRenderer legs;
+    [Header("鞋子")]
+    public SpriteRenderer boots;
+    [Header("右手武器")]
+    public SpriteRenderer hand_right;
+    [Header("左手盾牌")]
+    public SpriteRenderer hand_left;
+    [Header("披风")]
+    public SpriteRenderer cloak;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
