17f9d5b458631c14a6bd6c6f173507e40c5241e9
UNIEN <unien7415@gmail.com>
2024-03-12 22:52:38 +0900

    【】

---
+++ Assets/Scripts/Player/CharacterAppearanceChanges.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 关于改变玩家外形相关的程序
+/// </summary>
+public class CharacterAppearanceChanges : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerData_SO.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "PlayerData_SO", menuName = "Data_SO/Player")]
+public class PlayerData_SO : ScriptableObject
+{
+    [Title("玩家ID与名称")]
+    public int PlayerID;
+    public string playerName;//名称
+    public Sprite playerIcon;//图标
+    public Sprite playerOnWorldSprite;//游戏内画像
+    [Header("角色说明")]
+    [LabelWidth(100)]
+    [TextArea] public string playerDescription;//介绍
+    [Header("持有金")]
+    public int playerMoney;
+    [Space(10)]
+
+    [Header("玩家属性")]
+    public int playerMaxHP;//最大血量
+}
