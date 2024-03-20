81e00f07c0681c50f866ad3eeb1766763abb5a5b
UNIEN <unien7415@gmail.com>
2024-03-20 15:47:06 +0900

    【发现问题】寻路算法和地牢生成的生命周期冲突。需要改写

---
+++ Assets/Scripts/Edgar/MyCustomPostProcessingComponent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Edgar.Unity
+{
+    public class MyCustomPostProcessingComponent : DungeonGeneratorPostProcessingComponentGrid2D
+    {
+        public override void Run(DungeonGeneratorLevelGrid2D level)
+        {
+            // Implement the logic here
+
+        }
+    }
+}
